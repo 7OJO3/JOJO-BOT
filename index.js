5f63b0fd687a56a266723dd55505e5ecaafda543
KOOJOOOOO3 <jojoalrashidii@gmail.com>
2026-06-25 22:54:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot || !message.member?.roles.cache.has(ROLE_ID) || isProcessing.has(message.author.id))
         return;
 }
-});
+
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot || !message.member?.roles.cache.has(ROLE_ID) || isProcessing.has(message.author.id)) return;
 
